9a2180a131d1f3eb540d7a07988d14aa581e32f2
Sujal Raj <sujalraj2064@gmail.com>
2025-02-17 00:13:40 +0530

    Login and signup route frontend completed and form value consoled

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+
+function Navbar() {
+    const router = useRouter();
+    const LoginPageRedirect = () => {
+        router.push("/login");
+    };
+  return (
+    <nav className="h-[10vh] flex justify-between items-center px-10">
+        <h1 className="text-2xl/7 font-bold text-white sm:truncate sm:text-3xl sm:tracking-tight">InSights</h1>
+      <button 
+        onClick={LoginPageRedirect}
+      className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+        Login
+      </button>  
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+
+function SignUpPage() {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const signupUser =  (e:any) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <>
+    <nav className="h-[10vh] flex justify-between items-center px-10">
+        <h1 className="text-2xl/7 font-bold text-white sm:truncate sm:text-3xl sm:tracking-tight">InSights</h1>
+    </nav>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* <img
+            alt="Your Company"
+            src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-10 w-auto"
+          /> */}
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
+            Sign Up for an account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="username" className="block text-sm/6 font-medium">
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  required
+                  autoComplete="email"
+                  value={user.username}
+                  onChange={(e) =>
+                    setUser({ ...user, username: e.target.value })
+                  }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  value={user.email}
+                    onChange={(e) =>
+                        setUser({ ...user, email: e.target.value })
+                    }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium "
+                >
+                  Password
+                </label>
+                {/* <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div> */}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  value={user.password}
+                    onChange={(e) =>
+                        setUser({ ...user, password: e.target.value })
+                    }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                onClick={signupUser}
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-base/6 text-gray-500">
+            Already have an account?{" "}
+            {/* <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              Sign up now
+            </a> */}
+            <Link
+              href="/login"
+              className="font-semibold text-indigo-600 hover:text-indigo-500 uppercase"
+            >
+              Login Now
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SignUpPage;
